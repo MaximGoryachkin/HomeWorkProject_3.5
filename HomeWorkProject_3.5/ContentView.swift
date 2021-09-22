c61cadbb42acbeee3e6cb33eb18a393b842cb32d
Maxim Goryachkin <maxgoryachkin@mail.ru>
2021-09-22 06:56:58 +0300

    Initial Commit

---
+++ HomeWorkProject_3.5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWorkProject_3.5
+//
+//  Created by Максим on 22.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWorkProject_3.5/HomeWorkProject_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkProject_3_5App.swift
+//  HomeWorkProject_3.5
+//
+//  Created by Максим on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkProject_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
